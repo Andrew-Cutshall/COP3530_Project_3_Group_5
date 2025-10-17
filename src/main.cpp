4755834d57d4ba8d24b75daf4f4917631971e64a
Andrew-Cutshall <andrewcutshall03@gmail.com>
2025-10-17 18:25:20 -0400

    Add project files.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+//SFML Libraries
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+//A bunch of C++ built in libraries
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stdexcept> 
+#include <filesystem>
+#include <utility>
+#include <cstdlib>
+#include <thread>
+#include <stop_token>
+#include <chrono>
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+}
